05fc11063072ea413dcdbe4716b186dcb8f08f94
Denis Croitoru <dcroitor@tibco.com>
2023-02-09 19:12:48 +0200

    Add sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from './Header.svelte';
+</script>
+
+<div class="app">
+	
+	<Header></Header>
+
+	<main>
+		<slot />
+	</main>
+
+	
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let data;
+
+	let items = data.items;
+	
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>Set tour</h1>
+
+	{#each data.items as item}
+		<ul>
+			<li>
+				<a href="products/{item.id}">{item.title}</a>
+			</li>
+		</ul>
+	{/each}
+</section>
