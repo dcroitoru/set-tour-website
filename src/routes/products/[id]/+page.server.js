ab950ffe43666d70222c0a7b9891e9fc0f005a1e
Denis Croitoru <dcroitor@tibco.com>
2023-02-10 18:10:23 +0200

    Cleanup

--- src/routes/products/[id]/+page.server.js
+++
@@ -1,8 +0,0 @@
-export async function load({ fetch, params }) {
-	// console.log('should use fetch on params', params.id)
-
-	const response = await fetch(`https://fakestoreapi.com/products/${params.id}`);
-	const result = await response.json();
-
-	return result;
-}
